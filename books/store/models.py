779f214f24aa3d68a63a0e85d6cc58f478a47e74
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-30 16:31:00 +0300

    Add permissions and tests.

--- books/store/models.py
+++ books/store/models.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(decimal_places=2, max_digits=7)
     author_name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL,
+                              null=True)
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
