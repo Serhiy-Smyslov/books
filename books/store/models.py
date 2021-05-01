5852367c78c40a5388119a72ceb041dcc70b5120
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-05-01 19:53:24 +0300

    Add functionals like, rate and bookmarks.

--- books/store/models.py
+++ books/store/models.py
@@ -7,7 +7,28 @@ class Book(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=7)
     author_name = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL,
-                              null=True)
+                              null=True, related_name='my_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation',
+                                     related_name='books')
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, 'Ok'),
+        (2, 'Fine'),
+        (3, 'Good'),
+        (4, 'Amazing'),
+        (5, 'Incredible'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES, null=True)
+
+    def __str__(self):
+        return f'Id {self.user.username}: {self.book}, {self.rate}'
